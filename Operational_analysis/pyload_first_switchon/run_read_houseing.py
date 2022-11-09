74f1e083b6fb42d9167a0929f31fe09994afcb8e
lindamegner <linda@misu.su.se>
2022-11-09 10:02:46 +0100

    Added sorting files (forgot to add them to git in last push)

---
+++ Operational_analysis/pyload_first_switchon/run_read_houseing.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 13:53:36 2022
+
+@author: lindamegner
+
+
+This is more or less a duplicate of read_and_calibrate_all_files_in_directory but it is meant to be used as a script.
+"""
+
+import matplotlib.pyplot as plt
+
+from mats_l1_processing.get_temperature import create_temperature_info_array
+#import rawdata.time_tools as time_tools
+
+
+def plot_full_temperature_info(temperaturedata, relativetimedata):
+    HTR1A = temperaturedata[:, 0]
+    HTR1B = temperaturedata[:, 1]
+    HTR2A = temperaturedata[:, 2]
+    HTR2B = temperaturedata[:, 3]
+    HTR8A = temperaturedata[:, 4]
+    HTR8B = temperaturedata[:, 5]
+
+    plt.plot(relativetimedata / 60.0, HTR1A,
+             label="splitter plate, regulation")
+    plt.plot(relativetimedata / 60.0, HTR1B, label="splitter plate, measuring")
+    plt.plot(relativetimedata / 60.0, HTR2A, label="limb house, regulation")
+    plt.plot(relativetimedata / 60.0, HTR2B, label="limb house, measuring")
+    plt.plot(relativetimedata / 60.0, HTR8A, label="UV2 CCDn")
+    plt.plot(relativetimedata / 60.0, HTR8B, label="UV1 CCDn")
+    plt.xlabel("Time since epoch [min]")
+    plt.ylabel("Temperature [C]")
+    plt.legend()
+    plt.show()
+    plt.savefig("HTRmeasurements.jpg")
+
+
+
+# binned images with nskip
+#directory = '/Users/lindamegner/MATS/retrieval/git/MATS-L1-processing/testdata/binning_test_20200812/RacFiles_out/'
+directory='/Users/lindamegner/MATS/retrieval/FlightData/221106_payload_first_switchon/RacFiles_out'
+
+#add_rac_temp_data(directory + "/HTR.csv", CCDitem, labtemp=999)
+temperaturedata, relativetimedata = create_temperature_info_array(directory + "/HTR.csv")
+
+# utctime = []
+# for i in range(len(relativetimedata)):
+#     utctime.append(time_tools.onboardTime_to_utc(relativetimedata[i]))
+
+#plot_full_temperature_info(temperaturedata,utctime)
+
+plot_full_temperature_info(temperaturedata, relativetimedata)
