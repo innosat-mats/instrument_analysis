7fc11715c96962ec7e63567fd3b015ed66c8e924
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2022-11-15 16:56:43 +0100

    updated comissioning plan improved straylight analysis

--- Operational_analysis/early_data_harvest/straylight_analysis/straylight.py
+++ Operational_analysis/early_data_harvest/straylight_analysis/straylight.py
@@ -12,7 +12,7 @@ directory='../data/Pass74/'
 calibration_file='../calibration_data_linda.toml'
 
 _,df = read_CCDdata(directory)
-df = df[df.CCDSEL == 3]
+df = df[df.CCDSEL == 6]
 CCDitems = read_CCDitems(directory,items=df.to_dict('records'))
 
 #%%
@@ -29,20 +29,27 @@ for CCDitem in CCDitems:
         errors
     ) = L1_calibrate(CCDitem, instrument)
 #%%
-average_image = np.zeros(CCDitems[0]["image_calibrated"].shape)
+average_image_calib = np.zeros(CCDitems[0]["image_calibrated"].shape)
+average_image = np.zeros(CCDitems[0]["IMAGE"].shape)
+
 for i in range(len(CCDitems)):
-    average_image = average_image + CCDitems[i]["image_calibrated"]
+    average_image_calib = average_image_calib + CCDitems[i]["image_calibrated"]
+    average_image = average_image + CCDitems[i]["IMAGE"]
 
+average_image_calib = average_image_calib/len(CCDitems)
+average_column_calib = np.mean(average_image_calib[:,20:21],1)
 average_image = average_image/len(CCDitems)
-average_column = np.mean(average_image[:,20:30],1)
-
+average_column = np.mean(average_image[:,20:21],1)
 #%%
 z = np.arange(0,255)/4+50
-y = 0.8e6*np.exp(-z/10)
+y = average_column_calib[80]*np.exp(-(z-70)/8.5)
 
 plt.plot(y,z)
+plt.plot(average_column_calib,z)
 plt.plot(average_column,z)
 
+plt.legend(['rayleigh','calib','raw'])
 plt.xscale('log')
 
 # %%
+
--- Operational_analysis/early_data_harvest/time_skip_issue/time_jump_analysis.py
+++ Operational_analysis/early_data_harvest/time_skip_issue/time_jump_analysis.py
@@ -21,100 +21,161 @@ import numpy as np
 import pandas as pd
 
 
-orbit = 75
-directory='/home/olemar/Projects/Universitetet/MATS/instrument_analysis/Operational_analysis/early_data_harvest/data/Pass' + str(orbit) + '/'
+# orbit = 75
+# directory='/home/olemar/Projects/Universitetet/MATS/instrument_analysis/Operational_analysis/early_data_harvest/data/Pass' + str(orbit) + '/'
+# calibration_file='/home/olemar/Projects/Universitetet/MATS/instrument_analysis/Operational_analysis/early_data_harvest/calibration_data_linda.toml'
 
-calibration_file='/home/olemar/Projects/Universitetet/MATS/instrument_analysis/Operational_analysis/early_data_harvest/calibration_data_linda.toml'
+# instrument = Instrument(calibration_file)
 
-instrument = Instrument(calibration_file)
+# calibrate=False  
 
-calibrate=False  
+# _,df = read_CCDdata(directory)
 
-_,df = read_CCDdata(directory)
-
-df["EXP Date"] = pd.to_datetime(df["EXP Date"])
-df["TMHeaderTime"] = pd.to_datetime(df["TMHeaderTime"])
-
+# df["EXP Date"] = pd.to_datetime(df["EXP Date"])
+# df["TMHeaderTime"] = pd.to_datetime(df["TMHeaderTime"])
 
 
 
-#%%
-#Check how many images are lost
-fig, ax = plt.subplots(1, 1)
-TEXPIMS = [5.7,5.7,5.7,5.7,5.7,5.7,2]*2
-TEXPIMS = [10.7,10.7,10.7,10.7,10.7,10.7,4]
 
-import matplotlib.dates as mdates
-for CCD in [1,2,3,4,5,6,7]:
-    df_ccd = df.loc[df["CCDSEL"] == CCD]
-    TEXPIMS_CCD = TEXPIMS[CCD-1]
-
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-    ax.plot(df_ccd["EXP Date"],df_ccd["EXP Nanoseconds"].diff()*1e-9-TEXPIMS_CCD,'.',label=CCD)
-
-plt.xlabel('Time')
-plt.ylabel('Diff (time)')
-plt.title('Difference in EXPTIME Orbit ' + str(orbit))
-ax.legend()
-#plt.savefig('extime_jump_' + str(orbit) + '.png')
-
+# #%%
+# #Check how many images are lost
 # fig, ax = plt.subplots(1, 1)
-# ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-# ax.plot(df["TMHeaderTime"],df["TMHeaderNanoseconds"].diff()*1e-9,'.')
+# #TEXPIMS = [5.7,5.7,5.7,5.7,5.7,5.7,2]*2
+# TEXPIMS = [10.7,10.7,10.7,10.7,10.7,10.7,4]
+
+# import matplotlib.dates as mdates
+# for CCD in [1,2,3,4,5,6,7]:
+#     df_ccd = df.loc[df["CCDSEL"] == CCD]
+#     TEXPIMS_CCD = TEXPIMS[CCD-1]
+
+#     ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+#     ax.plot(df_ccd["EXP Date"],df_ccd["EXP Nanoseconds"].diff()*1e-9-TEXPIMS_CCD,'.',label=CCD)
+
 # plt.xlabel('Time')
 # plt.ylabel('Diff (time)')
+# plt.title('Difference in EXPTIME Orbit ' + str(orbit))
+# ax.legend()
+# #plt.savefig('extime_jump_' + str(orbit) + '.png')
 
-# fig, ax = plt.subplots(1, 1)
-# df["EXP Nanoseconds"].diff().hist(bins=50)
-# plt.title('EXP Nanoseconds difference')
-# plt.xlabel('Timedifference')
+# # fig, ax = plt.subplots(1, 1)
+# # ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+# # ax.plot(df["TMHeaderTime"],df["TMHeaderNanoseconds"].diff()*1e-9,'.')
+# # plt.xlabel('Time')
+# # plt.ylabel('Diff (time)')
 
-'''
-No data is lost, but there is a one second jump every now and then, this is
-due to SCET beeing set with every packet. Mikael Krus has suggested 
-two possible solutions (patches)
+# # fig, ax = plt.subplots(1, 1)
+# # df["EXP Nanoseconds"].diff().hist(bins=50)
+# # plt.title('EXP Nanoseconds difference')
+# # plt.xlabel('Timedifference')
 
-1: Fix this is post-processing
-2: Set SCET time only on instrument startup (subsec is set via HW signal anyway)
-'''
-# %%
+# '''
+# No data is lost, but there is a one second jump every now and then, this is
+# due to SCET beeing set with every packet. Mikael Krus has suggested 
+# two possible solutions (patches)
 
-CCD = 1
-df_ccd = df[df["CCDSEL"] == CCD]
-df_ccd = df_ccd.reset_index()
-TEXPIMS_CCD = TEXPIMS[CCD-1]
-timediff = df_ccd["EXP Nanoseconds"].diff()*1e-9-TEXPIMS_CCD
-timeshift_index = timediff[np.abs(timediff)>0.5].index
-long_stab_time = np.diff(timeshift_index).argmax()
+# 1: Fix this is post-processing
+# 2: Set SCET time only on instrument startup (subsec is set via HW signal anyway)
+# '''
+# # %%
 
-df_ccd[timeshift_index[long_stab_time-1]:timeshift_index[long_stab_time]]["EXP Nanoseconds"]
+# CCD = 1
+# df_ccd = df[df["CCDSEL"] == CCD]
+# df_ccd = df_ccd.reset_index()
+# TEXPIMS_CCD = TEXPIMS[CCD-1]
+# timediff = df_ccd["EXP Nanoseconds"].diff()*1e-9-TEXPIMS_CCD
+# timeshift_index = timediff[np.abs(timediff)>0.5].index
+# long_stab_time = np.diff(timeshift_index).argmax()
+
+# df_ccd[timeshift_index[long_stab_time-1]:timeshift_index[long_stab_time]]["EXP Nanoseconds"]
 
 
-correct_time = df_ccd["EXP Nanoseconds"].loc[timeshift_index[long_stab_time]:timeshift_index[long_stab_time+1]-1]
+# correct_time = df_ccd["EXP Nanoseconds"].loc[timeshift_index[long_stab_time]:timeshift_index[long_stab_time+1]-1]
 
-plt.plot(np.diff(correct_time*1e-9),'.')
+# plt.plot(np.diff(correct_time*1e-9),'.')
 
-#%%
-for i in range(0,long_stab_time):
-    index_of_shift_end = timeshift_index[long_stab_time-i]
-    index_of_shift_start = timeshift_index[long_stab_time-i-1]
-    print(str(index_of_shift_start) + ' ' + str(index_of_shift_end))
-    for j in range(index_of_shift_start,index_of_shift_end):
-        print(df_ccd.at[j,"EXP Nanoseconds"])
-        df_ccd.at[j,"EXP Nanoseconds"] = df_ccd.at[j,"EXP Nanoseconds"]-timediff[j]
-        print(df_ccd.at[j,"EXP Nanoseconds"])
+# #%%
+# for i in range(0,long_stab_time):
+#     index_of_shift_end = timeshift_index[long_stab_time-i]
+#     index_of_shift_start = timeshift_index[long_stab_time-i-1]
+#     print(str(index_of_shift_start) + ' ' + str(index_of_shift_end))
+#     for j in range(index_of_shift_start,index_of_shift_end):
+#         print(df_ccd.at[j,"EXP Nanoseconds"])
+#         df_ccd.at[j,"EXP Nanoseconds"] = df_ccd.at[j,"EXP Nanoseconds"]-timediff[j]*1e9
+#         print(df_ccd.at[j,"EXP Nanoseconds"])
 
-for i in range(long_stab_time+1,len(timeshift_index)-1):
-    index_of_shift_end = timeshift_index[i+1]
-    index_of_shift_start = timeshift_index[i]
-    print(str(index_of_shift_start) + ' ' + str(index_of_shift_end))
-    for j in range(index_of_shift_start,index_of_shift_end+1):
-        print(df_ccd.at[j,"EXP Nanoseconds"])
-        df_ccd.at[j,"EXP Nanoseconds"] = df_ccd.at[j,"EXP Nanoseconds"]-1e9
-        print(df_ccd.at[j,"EXP Nanoseconds"]) 
-# %%
+# for i in range(long_stab_time+1,len(timeshift_index)-1):
+#     index_of_shift_end = timeshift_index[i+1]
+#     index_of_shift_start = timeshift_index[i]
+#     print(str(index_of_shift_start) + ' ' + str(index_of_shift_end))
+#     for j in range(index_of_shift_start,index_of_shift_end):
+#         print(df_ccd.at[j,"EXP Nanoseconds"])
+#         df_ccd.at[j,"EXP Nanoseconds"] = df_ccd.at[j,"EXP Nanoseconds"]-timediff[j]*1e9
+#         print(df_ccd.at[j,"EXP Nanoseconds"]) 
+# # %%
 
-timediff = df_ccd["EXP Nanoseconds"].diff()*1e-9-TEXPIMS_CCD
-plt.plot(timediff[0:10])
+# plt.show()
+# timediff_2 = df_ccd["EXP Nanoseconds"].diff()*1e-9-TEXPIMS_CCD
+# plt.plot(timediff)
+# plt.plot(timediff_2,'.')
+# plt.ylabel('Difference in time between images [s]')
+# plt.xlabel('Image number')
+# plt.legend(['Before correction','After correction'])
+# plt.title('Timestamp stability CCD ' + str(CCD) + ' pass: ' + str(orbit))
+# plt.show()
 
-# %%
+# print(np.max(np.abs(timediff_2))<1e-3)
+# print(np.max(np.abs(timediff))<1e-3)
+# # %%
+
+
+
+def test_timecorrection():
+
+    orbit = 75
+    directory='/home/olemar/Projects/Universitetet/MATS/instrument_analysis/Operational_analysis/early_data_harvest/data/Pass' + str(orbit) + '/'
+
+    _,df = read_CCDdata(directory)
+    df["EXP Date"] = pd.to_datetime(df["EXP Date"])
+    df["TMHeaderTime"] = pd.to_datetime(df["TMHeaderTime"])
+    TEXPIMS = [10.7,10.7,10.7,10.7,10.7,10.7,4]
+    tolerance = 0.5 #tolerance of 0.5s
+    
+    for CCD in [1,2,3,4,5,6,7]:
+        df_ccd = df[df["CCDSEL"] == CCD]
+        df_ccd = df_ccd.reset_index()
+        correct_time(df_ccd, TEXPIMS[CCD-1],0.5)
+        timediff = df_ccd["EXP Nanoseconds"].diff()*1e-9-TEXPIMS[CCD-1]
+        
+        #Check that no timediff is large than tolerance
+        assert (np.max(np.abs(timediff))<tolerance)
+
+
+
+def correct_time(df, expected_diff, tolerance):
+
+    #Calculate the difference between expected time 
+    #difference and true time difference.
+    timediff = df["EXP Nanoseconds"].diff()*1e-9-expected_diff
+    timeshift_index = timediff[np.abs(timediff)>tolerance].index
+    
+    #Find longest time where the timing is stable
+    long_stab_time = np.diff(timeshift_index).argmax()
+
+    #Correct backwards from longest time
+    for i in range(0,long_stab_time):
+        index_of_shift_end = timeshift_index[long_stab_time-i]
+        index_of_shift_start = timeshift_index[long_stab_time-i-1]
+        for j in range(index_of_shift_start,index_of_shift_end):
+            df.at[j,"EXP Nanoseconds"] = df.at[j,"EXP Nanoseconds"]-timediff[j]*1e9
+
+    #Correct forward from longest time
+    for i in range(long_stab_time+1,len(timeshift_index)-1):
+        index_of_shift_end = timeshift_index[i+1]
+        index_of_shift_start = timeshift_index[i]
+        for j in range(index_of_shift_start,index_of_shift_end):
+            df.at[j,"EXP Nanoseconds"] = df.at[j,"EXP Nanoseconds"]-timediff[j]*1e9
+
+    return df
+
+
+test_timecorrection()
