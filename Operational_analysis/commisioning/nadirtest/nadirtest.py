c6c58aab0c8c262fca4b3f6ed84af6c0c6fbb069
lindamegner <linda@misu.su.se>
2022-11-24 11:21:38 +0100

    changed path in nadirtest to be relative

--- Operational_analysis/commisioning/nadirtest/nadirtest.py
+++ Operational_analysis/commisioning/nadirtest/nadirtest.py
@@ -8,15 +8,18 @@ from mats_l1_processing.L1_calibrate import L1_calibrate
 from mats_l1_processing.experimental_utils import plot_CCDimage
 
 # %%
-main_path='/Users/lindamegner/MATS/retrieval/'
+main_path='/Users/lindamegner/MATS/retrieval/git/'
 directory='/Users/lindamegner/MATS/retrieval/FlightData/commissioning/nadirtest/'
 #directory=main_path+'FlightData/221122_commisioning_day1/RacOut_darkcurrent_265_266/'
-calibration_file=main_path+'git/MATS-L1-processing/scripts/calibration_data_linda.toml'
-instrument_analysis_path=main_path+'git/instrument_analysis/'
-run_from_path=instrument_analysis_path+'Operational_analysis/commisioning/DarkCurrentAnalysis/'
+calibration_file=main_path+'MATS-L1-processing/scripts/calibration_data_linda.toml'
+instrument_analysis_path=main_path+'instrument_analysis/'
+run_from_path=instrument_analysis_path+'Operational_analysis/commisioning/nadirtest/'
 image_path=run_from_path+'images/'
 _,df = read_CCDdata(directory)
-df = df[df.TEXPMS == 32000]
+df = df[(df.NCOL>500)]
+#df=df.iloc[0::30, :]
+
+#%%
 CCDitems = read_CCDitems(directory,items=df.to_dict('records'))
 
 #%%
@@ -34,25 +37,24 @@ for CCDitem in CCDitems:
     ) = L1_calibrate(CCDitem, instrument)
 #%%
 
-calibrated=True
+calibrated=False
 if calibrated:
     image_specification='image_calibrated'
 else:
     image_specification='IMAGE'
 
-clim=[]
+
 meanvalue=[]
 exptime=[]
 for CCDitem in CCDitems:
     fig , ax= plt.subplots(1, 1, figsize=(8, 2))
     image=CCDitem[image_specification]
-    sp=plot_CCDimage(image, fig, ax, title=CCDitem['channel']+' '+str(CCDitem['TEXPMS']/1000),clim=clim)
+    sp=plot_CCDimage(image, fig, ax, title=CCDitem['channel']+' '+str(CCDitem['TEXPMS']/1000))
     meanvalue.append(image.mean())
     exptime.append(CCDitem['TEXPMS']/1000)
 
 
-    if CCDitem['TEXPMS']==32000:
-        fig.savefig(image_path+CCDitem['id']+'_32s.png', dpi=1600)
+ 
 #%%
     
 plt.plot(exptime,meanvalue,'*')
@@ -64,6 +66,6 @@ plt.show()
 
 
 
-plt.savefig(image_path+CCDitem['id']+'.png', dpi=1600)
+#plt.savefig(image_path+CCDitem['id']+'.png', dpi=1600)
 
 # %%
