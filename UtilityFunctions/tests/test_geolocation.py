dbfb0f48674a3650c20bf431311bd26e0a545b48
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2022-12-01 14:40:54 +0100

    added second TLE

--- UtilityFunctions/tests/test_geolocation.py
+++ UtilityFunctions/tests/test_geolocation.py
@@ -4,6 +4,8 @@ import datetime as DT
 def test_satellite():
     date = DT.datetime(2022,11,24,13,30,0)#        2022-11-23 08:20:00                  
     satlat,satlon,satLT,nadir_sza,nadir_mza,TPlat,TPlon,TPLT,TPsza,TPssa = satellite.get_position(date)
+    date = DT.datetime(2022,12,1,13,30,0)#        2022-11-23 08:20:00                  
+    satlat,satlon,satLT,nadir_sza,nadir_mza,TPlat,TPlon,TPLT,TPsza,TPssa = satellite.get_position(date)
 
 if __name__ == "__main__":
     test_satellite()
