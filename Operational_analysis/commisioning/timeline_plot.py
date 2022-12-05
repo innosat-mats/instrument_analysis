1b37b04ac8cd94cf64ff1a246153937b0f301b78
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2022-12-05 10:18:26 +0100

    updated schedule and scheule plotting tools

--- Operational_analysis/commisioning/timeline_plot.py
+++ Operational_analysis/commisioning/timeline_plot.py
@@ -3,6 +3,6 @@ import rawdata.timeline_tools as timeline_tools
 import datetime as DT
 
 # %%
-directory='/home/ochri/Projects/MATS/instrument_analysis/Operational_analysis/commisioning/'
+directory='/home/olemar/Projects/Universitetet/MATS/instrument_analysis/Operational_analysis/commisioning/'
 df = timeline_tools.load_schedule(directory + 'timeline_schedule.csv')
 timeline_tools.plot_schedule(df)
