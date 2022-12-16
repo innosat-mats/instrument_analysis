78db0c9d4170f89fb83368a7296c64a50603e34f
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2022-12-16 13:25:28 +0100

    updated schedule file

--- Operational_analysis/commisioning/odin/check_odin.py
+++ Operational_analysis/commisioning/odin/check_odin.py
@@ -6,11 +6,18 @@ from plotting.plotCCD import simple_plot
 
 schedule = pd.read_csv('/home/olemar/Projects/Universitetet/MATS/instrument_analysis/Operational_analysis/commisioning/timeline_schedule.csv')
 
-start_time = DT.datetime.strptime(schedule[schedule.name=="ODIN1"].start_date.values[0],'%Y-%m-%d %H:%M:%S')
-stop_time = DT.datetime.strptime(schedule[schedule.name=="ODIN1"].end_date.values[0],'%Y-%m-%d %H:%M:%S')
+# start_time = DT.datetime.strptime(schedule[schedule.name=="ODIN1"].start_date.values[0],'%Y-%m-%d %H:%M:%S')
+# stop_time = DT.datetime.strptime(schedule[schedule.name=="ODIN1"].end_date.values[0],'%Y-%m-%d %H:%M:%S')
+
+start_time = DT.datetime.strptime('2022/12/13 19:00:00','%Y/%m/%d %H:%M:%S')
+stop_time = DT.datetime.strptime('2022/12/13 20:00:00','%Y/%m/%d %H:%M:%S')
+
+
 #%%
 df = read_MATS_data(start_time,stop_time)
-simple_plot(df,'./',custom_cbar=True,ranges=[300,1000]) #plot_all_images
+#%%
+
+simple_plot(df,'./',custom_cbar=True,ranges=[500,20000]) #plot_all_images
 #%%
 
 
